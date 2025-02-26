1b8ef60e44fb9310f2a5728c49752ef71d23ecae
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-02-26 23:42:07 +0100

    modify javascript

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 function initMap() {
     const loc = { lat: 8.133050, lng: 4.238450 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 14,
-      center: loc
+        zoom: 14,
+        center: loc
     });
     new google.maps.Marker({ position: loc, map: map });
-  }
-  
+}
