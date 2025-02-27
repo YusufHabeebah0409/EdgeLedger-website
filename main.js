f9e499277479ccb9c13a493b5515b89c9249b08d
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-02-27 16:05:48 +0100

    sticky menu background

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ function initMap() {
     new google.maps.Marker({ position: loc, map: map });
 }
 
+// Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
 
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
@@ -17,7 +26,7 @@ $('#navbar a, .btn').on('click', function(event) {
 
         $('html, body').animate(
         { scrollTop: $(hash).offset().top - 100 },
-         800
+          800
     );
     }
 });
