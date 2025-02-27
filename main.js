f4e96197119f2f2d34128043778224656d8380d7
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-02-27 15:59:21 +0100

    smoth scrolling

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function initMap() {
     });
     new google.maps.Marker({ position: loc, map: map });
 }
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+        { scrollTop: $(hash).offset().top - 100 },
+         800
+    );
+    }
+});
